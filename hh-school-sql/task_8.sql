e47f760a975a6bbdf2b2b3b4b3211d4ca54502b6
Nik <ishmametev@phystech.edu>
2019-12-23 23:54:44 +0300

    task 8

--- hh-school-sql/task_8.sql
+++ hh-school-sql/task_8.sql
@@ -1,42 +1,35 @@
---
-CREATE TABLE resume_history (
-resume_id integer not null,
-last_change_time timestamp not null,
-old_title varchar(220),
-new_title varchar(220)
-);
+-- Написать запрос, в котором по resume_id выводилась бы история изменения названия резюме в виде: (resume_id, last_change_time, old_title, new_title). Возможно выбрать одну из реализаций, если не можете выбрать, попросите вариант у лектора:
+-- Создать столбец active в таблице резюме. Написать триггер который при любом изменении строки из таблицы (DELETE, UPDATE) пометит изменяемую запись в таблице как active = False, и при UPDATE создаст новую запись. Во всех запросах выше нужно будет учесть флаг active - то есть работать только с активными записями.
 
-CREATE OR REPLACE FUNCTION delete_resume() RETURNS TRIGGER AS $$
-    BEGIN
-        IF (TG_OP = 'DELETE') THEN
-        	UPDATE resume SET active = False, visible = False WHERE resume_id = OLD.resume_id;
-        	
-            IF NOT FOUND THEN RETURN NULL; END IF;
-            RETURN OLD;
-        END IF;
-    END;
-$$ LANGUAGE plpgsql;
+CREATE TABLE IF NOT EXISTS resume_history (
+	resume_id bigint not null,
+	last_change_time timestamp not null,
+	old_title varchar(220),
+	new_title varchar(220)
+);
 
 CREATE OR REPLACE FUNCTION update_resume() RETURNS TRIGGER AS $$
     BEGIN
-        IF (TG_OP = 'UPDATE') THEN  	
-        	INSERT INTO resume_history VALUES(NEW.resume_id, now(), OLD.title, NEW.title);
-            IF NOT FOUND THEN RETURN NULL; END IF;
-            RETURN NEW;
-        END IF;
+    IF TG_OP = 'UPDATE' THEN
+       INSERT INTO resume_history(resume_id, last_change_time, old_title, new_title)
+       VALUES (OLD.resume_id, now(), OLD.title, NEW.title);
+       OLD.resume_id = (SELECT MAX(resume_id)+1 FROM resume);
+       OLD.active = False;
+       OLD.visible = False;
+       INSERT INTO resume VALUES (OLD.*);
+       RETURN NEW;
+    ELSIF TG_OP = 'DELETE' THEN
+    	INSERT INTO resume_history(resume_id, last_change_time, old_title, new_title)
+    	VALUES (OLD.resume_id, now(), OLD.title, null);
+    	OLD.active = False;
+    	OLD.visible = False;
+    	INSERT INTO resume VALUES (OLD.*);
+    	RETURN OLD;
+	END IF;
     END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER func_delete_resume
-BEFORE UPDATE ON resume FOR EACH ROW EXECUTE FUNCTION delete_resume();
-
 CREATE TRIGGER func_update_resume
-AFTER UPDATE ON resume FOR EACH ROW EXECUTE FUNCTION update_resume();
+AFTER UPDATE OR DELETE ON resume FOR EACH ROW EXECUTE FUNCTION update_resume();
 
-select * from resume_history;
-select * from resume where init_id = 1;
-
-UPDATE resume SET visible = True where init_id = 1;
-DELETE FROM resume where resume.init_id=1;
-
-INSERT INTO resume VALUES(100005, now(), 3, True, True, 'Title 1', null, '', now());
+SELECT * FROM resume_history;
